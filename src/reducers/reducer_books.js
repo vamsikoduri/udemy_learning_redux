815c4f01b9c75beac657704e5a14918ce0cda682
vamsi krishna <vamc.koduri@gmail.com>
2017-09-25 06:21:00 +0530

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function () {
+
+    return [
+        { title: 'JavaScript: The Good parts'},
+        { title: 'Harry potter'},
+        { title: 'The Dark Tower'},
+        { title: 'Eloquent Ruby'}
+    ]
+
+
+}
